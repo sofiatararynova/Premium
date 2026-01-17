c74d7d21b8139157346b650dca8bdaebb4a13432
sojuuuuuuuuuuuu <sojuuuuuuuuuuuu@yandex.ru>
2026-01-17 10:29:30 +0300

    Содание PDF документ

--- Premium/Form1.cs
+++ Premium/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,73 @@ namespace Premium
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "PDF файлы|*.pdf";
+            save.FileName = "премия.pdf";
+
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                // Создаем PDF документ
+                PdfDocument document = new PdfDocument();
+                document.Info.Title = "Приказ";
+
+                // Добавляем страницу
+                PdfPage page = document.AddPage();
+                XGraphics gfx = XGraphics.FromPdfPage(page);
+
+                // шрифты
+                XFont fontNormal = new XFont("Arial", 12);
+                XFont fontLarge = new XFont("Arial", 20); 
+                XFont fontTitle = new XFont("Times New Roman", 24);
+
+                // Пишем текст на странице
+                gfx.DrawString("Приказ № 12 " +
+                    "\nО лишении премии работника ",
+                    fontTitle,
+                    XBrushes.Black,
+                    new XPoint(50, 50));
+
+                gfx.DrawString($"Дата: {DateTime.Now:dd.MM.yyyy}",
+                    fontNormal,
+                    XBrushes.Black,
+                    new XPoint(50, 70));
+
+                gfx.DrawString("В связи с ",
+                    fontNormal,
+                    XBrushes.Black,
+                    new XPoint(50, 100));
+
+               
+                gfx.DrawString("приказываю:",
+                    fontNormal, 
+                    XBrushes.Black,
+                    new XPoint(50, 130));
+
+                // Список задач
+                string[] tasks = {
+                    "1. ",
+                    "2. ",
+                    "3. "
+                };
+
+                int y = 160;
+                foreach (var task in tasks)
+                {
+                    gfx.DrawString(task, fontNormal, XBrushes.Black,
+                        new XPoint(70, y));
+                    y += 30;
+                }
+
+                // Сохраняем PDF
+                document.Save(save.FileName);
+                document.Close();
+
+                MessageBox.Show($"PDF создан: {save.FileName}");
+            }
         }
     }
 }
